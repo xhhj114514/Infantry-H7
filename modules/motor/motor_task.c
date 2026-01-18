043dc5d169bc8b0bed68eb8db8efb14626ff45b8
xhhj114514 <2669830090@qq.com>
2026-01-18 15:43:16 +0800

    modified:   application/robot_task.h 	modified:   modules/motor/motor_task.c

--- modules/motor/motor_task.c
+++ modules/motor/motor_task.c
@@ -1,5 +1,6 @@
 #include "motor_task.h"
 #include "dji_motor.h"
+#include "lk_motor.h"
 // #include "servo_motor.h"
 // #include "mi_motor.h"
 void MotorControlTask()
@@ -7,6 +8,7 @@ void MotorControlTask()
     // static uint8_t cnt = 0; 
     // if(cnt%5==0) //200hz
     DJIMotorControl();
+    LKMotorControl();
     // if(cnt%10==0) //100hz
     // MiMotorControl();
 
